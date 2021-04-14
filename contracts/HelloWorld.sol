1da561b039cd90e3246a60835165a19c87d494dd
Zach Mead <zachary.n.mead@gmail.com>
2021-04-14 11:59:58 -0500

    Waiting on ETH

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Unlicensed
 // Specifies the version of Solidity, using semantic versioning.
 // Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
 pragma solidity ^0.7.3;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 async function main() {
     const HelloWorld = await ethers.getContractFactory("Hello World");
 
-    // Start deployment, returning a promist that resolves to a contract object
+    // Start deployment, returning a promise that resolves to a contract object
     const hello_world = await HelloWorld.deploy("Hello World!");
     console.log("Contract deployed to address:", hello_world.address);
 }
