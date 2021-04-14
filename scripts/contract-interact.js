8590b20848fd84d69659f6ef9994a09ab1b0cc70
Zach Mead <zachary.n.mead@gmail.com>
2021-04-14 14:09:01 -0500

    Reading message

--- scripts/contract-interact.js
+++ scripts/contract-interact.js
@@ -6,3 +6,12 @@ const web3 = createAlchemyWeb3(API_URL);
 
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
 // console.log(JSON.stringify(contract.abi));
+
+const contractAddress = "0x27D1879D7BE8459d3110d2bd7ca86dFBD38E05b3";
+const helloWorldContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function main() {
+    const message = await helloWorldContract.methods.message().call();
+    console.log("The message is: " + message);
+  }
+  main();
